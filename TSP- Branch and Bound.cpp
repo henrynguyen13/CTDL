d0cd30a56095bb6c6b6f5682073ed29482d99139
Henry Nguyen <85147504+henrynguyen13@users.noreply.github.com>
2022-03-13 22:54:58 +0700

    Update TSP- Branch and Bound.cpp

--- TSP- Branch and Bound.cpp
+++ TSP- Branch and Bound.cpp
@@ -1,3 +1,23 @@
+Một người du lịch xuất phát từ thành phố thứ nhất muốn đi thăm quan tất cả n−1 thành phố khác. mỗi thành phố đúng một lần, rồi quay trở lại thành phố xuất phát.
+
+Yêu cầu: Cho biết chi phí đi lại giữa các thành phố, hãy giúp người du lịch tìm hành trình với tổng chi phí là nhỏ nhất.
+
+Input
+Dòng đầu tiên chứa hai số nguyên dương n,m cách nhau bởi dấu cách (n≤20,m<400).
+
+m dòng tiếp theo mỗi dòng chứa ba số nguyên dương i,j,c (i,j≤n,c≤10^6) biểu thị chi phí đi trực tiếp từ thành phố i đến thành phố j là c.
+
+Lưu ý: nếu từ thành phố i đến thành phố j nào không mô tả chi phí đi lại thì có nghĩa là không có đường đi trực tiếp từ i đến j.
+
+Output
+Ghi ra duy nhất một số là tổng chi phí hành trình nhỏ nhất tìm được.
+	
+Ex: Input: 
+2 2
+1 2 3
+2 1 2
+Output: 5
+
 #include<stdio.h>
 #define N 30
 #define INF 1000000
